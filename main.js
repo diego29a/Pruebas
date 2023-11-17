3618142fac9168eb3aeb89cdc1e7c7751277c2f9
Diego <diegobermejocalvo@gmail.com>
2023-11-17 20:10:24 +0100

    add

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 //Práctica 1
 let msg = "Hola Mundo"
 
-console.log("Hola Mundo");
+console.log(msg)
 
 document.addEventListener("DOMContentLoaded", init)
 
 function init(event) {
     let title = document.getElementById("title")
     title.style.color = "red"
+    title.innerHTML = msg
 }
 
 
