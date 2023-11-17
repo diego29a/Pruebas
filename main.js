93c213497aa65b287d1784143d83888ae58b54d0
Diego <diegobermejocalvo@gmail.com>
2023-11-17 20:00:27 +0100

    add

---
+++ main.js
@@ -0,0 +1,13 @@
+//Práctica 1
+let msg = "Hola Mundo"
+
+console.log("Hola Mundo");
+
+document.addEventListener("DOMContentLoaded", init)
+
+function init(event) {
+    let title = document.getElementById("title")
+    title.style.color = "red"
+}
+
+
