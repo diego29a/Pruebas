d7cc732ec5327a475f71562ecb35a273277e4e56
Diego <diegobermejocalvo@gmail.com>
2023-11-30 19:28:13 +0100

    add

---
+++ main.js
@@ -0,0 +1,11 @@
+import {secundaria} from "./practica1.js"
+
+let principal = 100
+
+function saludar() {
+    let resultado = document.getElementById("resultado")
+    let nombre = document.getElementById("campoNombre")
+    resultado.innerHTML = nombre.value
+}
+
+
